6a93042a0fa74210b7043fff1d1a9fa5cdb2c47d
DevonMX <dw.jell@gmail.com>
2019-05-23 21:24:26 -0500

    Demo
    Prueba DEMO Resuelve

--- provoke.com/includes/_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new medoo([
-    'database_type' => 'mysql',
-    'database_name' => 'genotip1_mallsmx',
-    'server' => 'mallsmexico.com',
-    'username' => 'genotip1_malls',
-    'password' => ';n3w0XVVIHJ$',
-    'charset' => 'utf8'
-]);
